5757c75a67a9d6796838dc32109201a6161f3e92
yadhst <fayyadh.abdurrahman577@gmail.com>
2025-03-12 20:16:49 +0700

    Refactor layout to use Inter and Merriweather fonts.

---
+++ src/styles/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Merriweather } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+  variable: "--font-merriweather",
+});

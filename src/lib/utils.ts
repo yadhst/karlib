5e220a8496e3afe44a9481c2f33a230aa3c18ff2
yadhst <fayyadh.abdurrahman577@gmail.com>
2025-03-22 18:58:37 +0700

    Add utility function to retrieve the first search parameter from SearchParams

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { twMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "clsx";
+import type { SearchParams } from "next/dist/server/request/search-params";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getFirstSearchParams(searchParams: SearchParams, key: string) {
+  const value = searchParams[key];
+  if (!value) return null;
+
+  if (Array.isArray(value)) return value[0];
+  return value;
 }
